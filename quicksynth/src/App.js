9725d6403af47f6c8607f4b665674fab93ebce0d
Jack Blakeston <jackblakeston93@gmail.com>
2022-05-12 18:30:28 +0200

    boilerplate for presentation

--- quicksynth/src/App.js
+++ quicksynth/src/App.js
@@ -2,8 +2,6 @@ import { playControl, stopControl } from "./inputControl";
 
 const App = () => {
 
-  document.addEventListener('keydown', playControl)
-  document.addEventListener('keyup', stopControl)
 
   return (
     <div>QUICKSYNTH</div>
--- quicksynth/src/inputControl.js
+++ quicksynth/src/inputControl.js
@@ -5,22 +5,9 @@ import {playNote} from './synth'
 const activeNotes = {}
 
 export const playControl = (event) => {
-  const key = event.key
-  if (!event.repeat) {
-    if (keyboardMappings[key]) {
-      const note = keyboardMappings[key];
-      activeNotes[note] = playNote(noteFrequencies[note]);
-    }
-  }
+
 }
 
 export const stopControl = (event) => {
-  const key = event.key;
-  if (keyboardMappings[key]) {
-    const note = keyboardMappings[key];
-    if (activeNotes[note]) {
-      activeNotes[note]();
-      delete activeNotes[note];
-    }
-  }
+
 }
